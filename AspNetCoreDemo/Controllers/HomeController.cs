106e9d283dc10dc61c3f356c444d6e45d86e15f4
David Ebbo <david.ebbo@gmail.com>
2017-02-09 11:17:55 -0800

    Add some logging from Home controller Index action

--- AspNetCoreDemo/Controllers/HomeController.cs
+++ AspNetCoreDemo/Controllers/HomeController.cs
@@ -4,20 +4,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Logging;
 
 namespace AspNetCoreDemo.Controllers
 {
     public class HomeController : Controller
     {
         readonly IOptions<MyConfig> _config;
+        readonly ILogger _logger;
 
-        public HomeController(IOptions<MyConfig> config)
+        public HomeController(IOptions<MyConfig> config, ILoggerFactory loggerFactory)
         {
             _config = config;
+            _logger = loggerFactory.CreateLogger<HomeController>();
         }
 
         public IActionResult Index()
         {
+            _logger.LogInformation("Some information log");
+            _logger.LogWarning("This is a warning");
+            _logger.LogError("This is an error");
+
             return View();
         }
 
